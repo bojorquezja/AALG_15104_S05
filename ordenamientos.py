d0df64cfd9ada82e073e2a527a80c0f765065d70
bojorquezja <bojorquez.ja@gmail.com>
2026-04-20 20:17:33 -0500

    ord burbuja

--- ordenamientos.py
+++ ordenamientos.py
@@ -1,3 +1,11 @@
+def ordburbuja(lst):
+    n = len(lst)
+    for pasada in range(n-1):
+        for izqpar in range(n - 1 - pasada):
+            derpar = izqpar + 1
+            if lst[izqpar] > lst[derpar]:
+                lst[izqpar], lst[derpar] = lst[derpar], lst[izqpar]
+
 def ordseleccion(lst):
     n = len(lst)
     for manoizq in range(n-1):
@@ -10,5 +18,5 @@ def ordseleccion(lst):
 
 
 lista = [2,8,5,3,9,4,1]
-ordseleccion(lista)
+ordburbuja(lista)
 print(lista)
